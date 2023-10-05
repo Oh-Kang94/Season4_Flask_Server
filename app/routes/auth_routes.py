4ec51b1c76e32b9388e430039e25ea25312b06ec
Oh-Kang94 <okh19941994@naver.com>
2023-10-05 15:58:30 +0900

    need to fix jwt.revoked_token_loader

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -5,11 +5,12 @@ from ..services.user_service import UsersService
 from ..services.auth_service import Auth_Service
 from ..models.ApiModel import Login_fields
 
+
 def auth_routes(auth_ns):
     @auth_ns.route('/')
     class Login(Resource):
         @api.expect(Login_fields)
-        @auth_ns.doc(description = '로그인 하는 route',)  
+        @auth_ns.doc(description='로그인 하는 route',)
         def post(self):
             data = api.payload
             email = data['email']
@@ -19,24 +20,32 @@ def auth_routes(auth_ns):
             if user and user.password == password:
                 access_token = create_access_token(identity=email)
                 refresh_token = create_refresh_token(identity=email)
-                Auth_Service.set_refreshtoken(refreshtoken=refresh_token,email=email)
-                response =  {
+                Auth_Service.set_refreshtoken(
+                    refreshtoken=refresh_token, email=email)
+                response = {
                     'message': 'Logged in successfully',
-                    'name' : user.name,
-                    'nickname' : user.nickname
+                    'name': user.name,
+                    'nickname': user.nickname
                 },
                 # header를 태운다.
                 response_headers = {
-                'access_token': access_token,
-                'refresh_token': refresh_token,
+                    'access_token': access_token,
+                    'refresh_token': refresh_token,
                 }
                 return response, 200, response_headers
             return {'message': 'Invalid credentials'}, 401
+
     @auth_ns.route('/access')
     class Refresh(Resource):
         @jwt_required(refresh=True)
-        @auth_ns.doc(security = 'Bearer', description = 'ACCESS_TOKEN을 발급 받기 위함.',)  
+        @auth_ns.doc(security='Bearer', description='ACCESS_TOKEN을 발급 받기 위함.',)
         def get(self):
             current_user = get_jwt_identity()
             new_access_token = create_access_token(identity=current_user)
-            return {'access_token': new_access_token}, 200
+            response_headers = {
+                'access_token': new_access_token,
+            }
+            return {'create Success'}, 200, response_headers
+        @jwt.revoked_token_loader
+        def revoked_token_callback(jwt_header, jwt_payload):
+            return{'message': 'Invalid credentials'}, 401
